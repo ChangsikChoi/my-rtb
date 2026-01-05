923df910d7c9abb3bb0eccc0c27beb1eff7c84d8
ChangsikChoi <changsik.choi@a1mediagroup.co.kr>
2026-01-05 15:33:31 +0900

    Blocking 포인트 탐색을 위한 BlockHound 설정 추가

--- bidder/src/main/java/com/example/bidder/BiddingApplication.java
+++ bidder/src/main/java/com/example/bidder/BiddingApplication.java
@@ -2,11 +2,15 @@ package com.example.bidder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class BiddingApplication {
     public static void main(String[] args) {
+        BlockHound.install(builder ->
+            builder.allowBlockingCallsInside("io.lettuce.core.RedisClient", "connect")
+        );
         SpringApplication.run(BiddingApplication.class, args);
         Hooks.enableAutomaticContextPropagation();
     }
